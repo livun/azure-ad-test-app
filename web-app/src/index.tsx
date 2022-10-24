c4ab90293621add2bdcd1e1350fe0ce70184d9dd
Mustafa-Hersi <110983038+Mustafa-Hersi@users.noreply.github.com>
2022-10-24 13:33:11 +0200

    added configuration to index.tsx

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { msalConfig } from "./authConfig";
+import reportWebVitals from "./reportWebVitals";
+import { Configuration, PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+
+const config: Configuration = {
+  auth: {
+    clientId: msalConfig.auth.clientId,
+  },
+};
+
+const publicClientApplication = new PublicClientApplication(config);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={publicClientApplication}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
 
